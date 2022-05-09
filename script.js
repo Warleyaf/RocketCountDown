2df1d40965510ef775a14ee2b65438bb64b4680b
Warley Afonso <warleysilva365@gmail.com>
2022-05-09 20:01:09 -0300

    Refeito o código

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 const days = document.getElementById('days');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
-const dateTime = new Date('22 may 2022')
+const seconds = document.getElementById('seconds')
+
+const newtDate = new Date('25 may 2022')
+
+
 function countDown() {
-  const currentDate = new Date();
-  const eventDate = new Date(dateTime)
-  const totalSeconds = (eventDate - currentDate) / 1000;
 
-  const eventDays = Math.floor((totalSeconds / 3600)/24);
-  const eventHours = Math.floor((totalSeconds / 3600)%24);
-  const eventMinutes = Math.floor((totalSeconds / 60)%60);
-  const eventSeconds = Math.floor(totalSeconds % 60);
+  const newDateEvent = new Date(newtDate);
+  const currentDate = new Date()
 
-  days.innerHTML = eventDays
-  hours.innerHTML = formatTime(eventHours);
-  minutes.innerHTML = formatTime(eventMinutes);
-  seconds.innerHTML = formatTime(eventSeconds);
+  const totalSeconds = (newDateEvent - currentDate) / 1000;
+  const eventDays = Math.floor((totalSeconds / 3600) / 24);
+  const eventHours = Math.floor(totalSeconds / 3600) % 24;
+  const eventMinutes = Math.floor(totalSeconds / 60) % 60;
+  const eventSeconds = Math.floor(totalSeconds) % 60;
+  
+  days.innerHTML = formatTime(eventDays)
+  hours.innerHTML = formatTime(eventHours)
+  minutes.innerHTML = formatTime(eventMinutes)
+  seconds.innerHTML = formatTime(eventSeconds)
 
   function formatTime(time) {
     return time < 10 ? `0${time}` : time;
   }
-  
+
 }
 
 countDown()
-
-setInterval(countDown, 1000);
+setInterval(countDown,1000);
